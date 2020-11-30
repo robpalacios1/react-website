f88da2a76872e403b6346ca746d993f0310e1846
roberto <robpalacios1@hotmail.com>
2020-11-30 13:52:44 -0500

    remove some spaces

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import Button from './Button';
+import './Navbar.css';
 
 const Navbar = () => {
 
@@ -30,7 +31,7 @@ const Navbar = () => {
             <nav className="navbar">
                 <div className="navbar-container">
                     <Link to="/" className="navbar-logo">
-                        TRVL <i className="fab fa-typo3"></i>
+                        ROBERTO PALACIOS <i className="fab fa-typo3"></i>
                     </Link>
                     <div className="menu-icon" onClick={handleClick}>
                         <i className={click ? 'fas fa-times' : 'fas fa-bars'} />

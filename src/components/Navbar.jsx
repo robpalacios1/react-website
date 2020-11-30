3662c9518b5eb3de9d1762602eeef1ee8c08f8ca
roberto <robpalacios1@hotmail.com>
2020-11-30 15:58:13 -0500

    add Link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import Button from './Button';
 import './Navbar.css';
@@ -24,14 +24,18 @@ const Navbar = () => {
         }
     }
 
+    useEffect(() => {
+        showButton();
+    }, [])
+
     window.addEventListener('resize', showButton)
 
     return (
         <>
             <nav className="navbar">
                 <div className="navbar-container">
-                    <Link to="/" className="navbar-logo">
-                        ROBERTO PALACIOS <i className="fab fa-typo3"></i>
+                    <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+                        ROBERTO <i className="fab fa-typo3"></i>
                     </Link>
                     <div className="menu-icon" onClick={handleClick}>
                         <i className={click ? 'fas fa-times' : 'fas fa-bars'} />

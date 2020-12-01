984e1c28fcc449c6ee22b8ae8647f9121ca725b4
roberto <robpalacios1@hotmail.com>
2020-11-30 23:16:36 -0500

    erase some spaces

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -3,6 +3,7 @@ import '../../App.css'
 import HeroSection from '../HeroSection';
 import Cards from '../Cards';
 import Footer from '../Footer';
+import '../Footer.css'
 
 const Home = () => {
     return (

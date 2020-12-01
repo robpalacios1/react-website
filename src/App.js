a79bec9355e884a768c0b1b50a23c7f37f219b6b
roberto <robpalacios1@hotmail.com>
2020-11-30 21:27:59 -0500

    import Services, Products, SignUp and add Route for each one

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ import {
   Route
 } from 'react-router-dom'
 import Navbar from './components/Navbar';
-import Home from './components/pages/Home'
+import Home from './components/pages/Home';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
 
 function App() {
   return (
@@ -15,6 +18,9 @@ function App() {
         <Navbar />
         <Switch>
           <Route path="/" exact component={Home} />
+          <Route path="/services" component={Services}/>
+          <Route path="/products" component={Products}/>
+          <Route path="/sign-up" component={SignUp}/>
         </Switch>
       </Router>
     </>
